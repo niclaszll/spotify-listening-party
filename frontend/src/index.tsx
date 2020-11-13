1b080989d90cbe535f61ad0dc22e8a835b29c1e5
niclaszll <zellpublic+github@icloud.com>
2020-11-13 21:55:49 +0100

    make redux store persistent

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,13 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import {
+  persistStore,
+} from 'redux-persist'
 import store from './store'
 import App from './App'
+
+const persistor = persistStore(store)
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,12 +1,35 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { configureStore, combineReducers, getDefaultMiddleware } from '@reduxjs/toolkit'
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import spotifyReducer from './modules/spotify/index'
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
 
 const rootReducer = combineReducers({
   spotify: spotifyReducer,
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
   devTools: process.env.NODE_ENV !== 'production',
 })
 
