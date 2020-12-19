e91ab0cd19731b133c57756573af604dbfc59c73
Niclas <zellpublic+github@icloud.com>
2020-12-19 17:41:47 +0100

    add listener preview to navbar in room

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,24 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useHistory, useLocation, Link } from 'react-router-dom'
 import { clearSpotifyState, selectSpotifyState } from '../../store/modules/spotify'
 import { ReactComponent as ArrowBack } from '../../img/icons/arrow_back.svg'
 import * as styles from './styles.module.sass'
+import { ReactComponent as People } from '../../img/icons/people.svg'
 
 export default function Navbar() {
   const history = useHistory()
   const location = useLocation()
   const dispatch = useDispatch()
-  const { token } = useSelector(selectSpotifyState)
+  const [listenerDetailsOpen, setListenerDetailsOpen] = useState<Boolean>(false)
+  const { token, currentRoom, user } = useSelector(selectSpotifyState)
 
   const handleClick = () => {
     dispatch(clearSpotifyState())
     history.push('/')
   }
-
-  useEffect(() => {
-    console.log(location)
-  }, [location])
 
   return (
     <div className={styles.container}>
@@ -31,9 +29,44 @@ export default function Navbar() {
         <div />
       )}
       {token !== null && (
-        <button type="button" onClick={handleClick}>
-          End Session
-        </button>
+        <div className={styles.right}>
+          {location.pathname.startsWith('/room') && (
+            <>
+              <div className={styles.listeners}>
+                <div className={styles.listenerCircle} title={user?.display_name}>
+                  {user?.display_name.charAt(0).toUpperCase()}
+                </div>
+                {currentRoom.activeListeners
+                  .filter((listener) => listener !== user?.display_name)
+                  .slice(0, 2)
+                  .map((listener) => (
+                    <div className={styles.listenerCircle} title={listener}>
+                      {listener.charAt(0).toUpperCase()}
+                    </div>
+                  ))}
+                <div
+                  className={styles.listenerCircle}
+                  onClick={() => setListenerDetailsOpen(!listenerDetailsOpen)}
+                  role="button"
+                  tabIndex={0}
+                  onKeyDown={() => {}}
+                >
+                  <People />
+                  {listenerDetailsOpen && (
+                    <div className={styles.listenerDetails}>
+                      {currentRoom.activeListeners.map((listener) => (
+                        <div>{listener}</div>
+                      ))}
+                    </div>
+                  )}
+                </div>
+              </div>
+            </>
+          )}
+          <button type="button" onClick={handleClick}>
+            End Session
+          </button>
+        </div>
       )}
     </div>
   )
--- frontend/src/components/Navbar/styles.module.sass.d.ts
+++ frontend/src/components/Navbar/styles.module.sass.d.ts
@@ -1,2 +1,6 @@
 export const container: string
 export const goBack: string
+export const right: string
+export const listeners: string
+export const listenerCircle: string
+export const listenerDetails: string
