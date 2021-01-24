501d09e1f8836af965338d0b0964777e2bbb8cf0
Niclas <zellpublic+github@icloud.com>
2021-01-24 12:20:09 +0100

    add more room tests and socket.io test boilerplate

--- api/tests/room.test.js
+++ api/tests/room.test.js
@@ -1,7 +1,7 @@
 import {describe, expect, beforeAll, afterAll, afterEach, it} from '@jest/globals'
 
 import {connect, clearDatabase, closeDatabase} from './db-handler.js'
-import {createRoom, findRoomById} from '../src/persistence/queries.js'
+import {createRoom, findRoomById, getAllRooms, updateRoom} from '../src/persistence/queries.js'
 
 const validRoom = {
   id: 'room_0001',
@@ -20,7 +20,7 @@ const validRoom2 = {
   id: 'room_0002',
   name: 'Valider Raum',
   roomPublic: true,
-  roomPassword: '',
+  roomPassword: 'password',
   activeListeners: [],
   queue: [],
   shuffledQueue: [],
@@ -29,13 +29,23 @@ const validRoom2 = {
   currentTrack: null,
 }
 
+const invalidRoom1 = {
+  id: 'room_0002',
+  roomPublic: true,
+  roomPassword: '',
+  activeListeners: [],
+  queue: [],
+  shuffledQueue: [],
+  shuffled: false,
+  creatorId: 'tester',
+  currentTrack: null,
+}
+
 /**
  * Connect to a new in-memory database before running any tests.
  */
 beforeAll(async () => {
   connect()
-  await createRoom(validRoom)
-  await createRoom(validRoom2)
 })
 
 /**
@@ -52,9 +62,73 @@ afterAll(async () => closeDatabase())
  * Room test suite.
  */
 describe('room ', () => {
-  it('should retrive rooms correctly', async () => {
+  it('should retrive room correctly', async () => {
+    await createRoom(validRoom)
+    await createRoom(validRoom2)
+
     const foundRoom = await findRoomById('room_0001', true)
     expect(foundRoom.id).toBe(validRoom.id)
     expect(foundRoom.id).not.toBe(validRoom2.id)
+  })
+
+  it('should not create invalid room', async () => {
+    try {
+      await createRoom(invalidRoom1)
+    } catch (e) {
+      // should throw error
+      expect(e._message).toMatch('Room validation failed')
+    }
+  })
+
+  it('should retrive all rooms correctly', async () => {
+    await createRoom(validRoom)
+    await createRoom(validRoom2)
+
+    const foundRooms = await getAllRooms()
+    expect(foundRooms.length).toBe(2)
+  })
+
+  it('should update room correctly', async () => {
+    await createRoom(validRoom)
+
+    const newPassword = 'pw'
+    const newShuffled = !validRoom.shuffled
+
+    await updateRoom(validRoom.id, {shuffled: newShuffled, roomPassword: newPassword})
+    const updatedRoom = await findRoomById(validRoom.id, true)
+
+    expect(updatedRoom.shuffled).toBe(newShuffled)
+    expect(updatedRoom.roomPassword).toBe(newPassword)
+  })
+
+  it('creatorId should not update if not empty string', async () => {
+    await createRoom(validRoom)
+
+    await updateRoom(validRoom.id, {creatorId: 'newUserName'})
+    const updatedRoom = await findRoomById(validRoom.id)
+
+    expect(updatedRoom.creatorId).toBe(validRoom.creatorId)
+  })
+
+  it('password projection should work correctly', async () => {
+    await createRoom(validRoom)
+    await createRoom(validRoom2)
+
+    const roomWithPassword = await findRoomById(validRoom2.id, true)
+    const roomWithoutPassword = await findRoomById(validRoom2.id)
+
+    const allRoomsWithPassword = await getAllRooms(true)
+    const allRoomsWithoutPassword = await getAllRooms()
+
+    // for findRoomById
+    expect(roomWithPassword.roomPassword).toBe(validRoom2.roomPassword)
+    expect(roomWithoutPassword.roomPassword).toBe(undefined)
+
+    // for getAllRooms
+    expect(allRoomsWithPassword[0].roomPassword).toBe(validRoom.roomPassword)
+    expect(allRoomsWithPassword[1].roomPassword).toBe(validRoom2.roomPassword)
+    allRoomsWithoutPassword.forEach((room) => {
+      expect(room.roomPassword).toBe(undefined)
+    })
   })
 })
