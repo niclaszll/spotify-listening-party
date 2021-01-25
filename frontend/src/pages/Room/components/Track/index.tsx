aa5a3ffb0c1efa22d8ee1499efcfd31f87fadd29
Niclas <zellpublic+github@icloud.com>
2021-01-25 12:19:26 +0100

    current song green + track duration

--- frontend/src/pages/Room/components/Track/index.tsx
+++ frontend/src/pages/Room/components/Track/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectSpotifyState } from '../../../../store/modules/spotify'
 import { WebPlaybackTrack } from '../../../../util/types/spotify'
 import * as styles from './style.module.sass'
 
@@ -9,11 +11,26 @@ type TrackProps = {
 export default function Track(props: TrackProps) {
   const { track } = props
 
+  const { playbackInfo } = useSelector(selectSpotifyState)
+
+  const formattedDuration = (duration: number) => {
+    const minutes = Math.floor(duration / (1000 * 60)) % 60
+    const seconds = Math.floor(duration / 1000) % 60
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`
+  }
+
   return (
     <div className={styles.container}>
-      <div className={styles.trackInfo}>
-        <div>{track.name}</div>
-        <div>{track.artists.map((artistObject) => artistObject.name).join(', ')}</div>
+      <div
+        className={`${styles.trackInfo} ${playbackInfo?.item.id === track.id && styles.playing}`}
+      >
+        <div className={styles.left}>
+          <div>{track.name}</div>
+          <div>{track.artists.map((artistObject) => artistObject.name).join(', ')}</div>
+        </div>
+        <div className={styles.right}>
+          <div>{formattedDuration(track.duration_ms)}</div>
+        </div>
       </div>
     </div>
   )
--- frontend/src/pages/Room/components/Track/style.module.sass.d.ts
+++ frontend/src/pages/Room/components/Track/style.module.sass.d.ts
@@ -1,2 +1,5 @@
 export const container: string
 export const trackInfo: string
+export const left: string
+export const right: string
+export const playing: string
